a775c66f6fe38eb0e2ee664745c57b2502f10d2d
zhislin <zhislin@icloud.com>
2025-03-27 17:20:33 +0800

    update case for assertion check

--- reportParser/src/test/java/com/github/jaksonlin/jacocoparser/util/TestDiffParser.java
+++ reportParser/src/test/java/com/github/jaksonlin/jacocoparser/util/TestDiffParser.java
@@ -5,6 +5,11 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import com.github.jaksonlin.jacocoparser.util.DiffParser;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Consumer;
+
 public class TestDiffParser {
 
 
@@ -19,16 +24,18 @@ public class TestDiffParser {
         System.out.println(diffInfo.removedLines);
         System.out.println(diffInfo.modifiedLines);
         System.out.println(diffInfo.unchangedLines);
-        // assert 2
-        Assert.assertEquals(diffInfo.addedLines.size(), 4);
-        Assert.assertEquals(diffInfo.removedLines.size(), 2);
-        Assert.assertEquals(diffInfo.modifiedLines.size(), 0);
-        Assert.assertEquals(diffInfo.unchangedLines.size(), 6);
+        // assert 2  
 
+        checkDiffInfos(diffInfo.addedLines, 4);
+        checkDiffInfos(diffInfo.removedLines, 2);
+        checkDiffInfos(diffInfo.modifiedLines, 0);
+        checkDiffInfos(diffInfo.unchangedLines, 6);
 
     }
 
-
+    private void checkDiffInfos(List<Integer> listToCheck, int expectedSize){
+        Assert.assertEquals(listToCheck.size(), expectedSize);
+    }
 
 
 
@@ -52,7 +59,7 @@ public class TestDiffParser {
 
 
 
-    @Test
+    @Test(expected = NullPointerException.class)
     public void testParseDiff_test2(){
         // step 1
         String diff = "@@ -24,8 +24,10 @@\n //= require g.raphael-min\n //= require g.bar-min\n //= require branch-graph\n-//= require highlightjs.min\n-//= require ace/ace\n //= require_tree .\n //= require d3\n //= require underscore\n+\n+function fix() { \n+  alert(\"Fixed\")\n+}";
@@ -62,11 +69,6 @@ public class TestDiffParser {
         System.out.println(diffInfo.modifiedLines);
         System.out.println(diffInfo.unchangedLines);
         // assert 1
-        Assert.assertEquals(diffInfo.addedLines.size(), 4);
-        Assert.assertEquals(diffInfo.removedLines.size(), 2);
-        Assert.assertEquals(diffInfo.modifiedLines.size(), 0);
-        Assert.assertEquals(diffInfo.unchangedLines.size(), 6);
-
 
     }
 
@@ -80,17 +82,20 @@ public class TestDiffParser {
         // step 1
         String diff = "@@ -24,8 +24,10 @@\n //= require g.raphael-min\n //= require g.bar-min\n //= require branch-graph\n-//= require highlightjs.min\n-//= require ace/ace\n //= require_tree .\n //= require d3\n //= require underscore\n+\n+function fix() { \n+  alert(\"Fixed\")\n+}";
         DiffParser.DiffInfo diffInfo = DiffParser.parseDiff(diff);
+
+        
         System.out.println(diffInfo.addedLines);
         System.out.println(diffInfo.removedLines);
         System.out.println(diffInfo.modifiedLines);
         System.out.println(diffInfo.unchangedLines);
         // assert 1
-        Assert.assertEquals(diffInfo.addedLines.size(), 4);
-        Assert.assertEquals(diffInfo.removedLines.size(), 2);
-        Assert.assertEquals(diffInfo.modifiedLines.size(), 0);
-        Assert.assertEquals(diffInfo.unchangedLines.size(), 6);
+        myTestCheckHelper(diffInfo.addedLines, 4);
 
 
+    }
+
+    private void myTestCheckHelper(List<Integer> check, int size){
+        Assert.assertEquals(check.size(), size);
     }
 
     @Test
